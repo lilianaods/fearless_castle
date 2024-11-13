7b2205ff85790fc55658cf4204f91b31878e2051
Liliana Silva <lilianaods@outlook.com>
2024-11-13 19:33:09 -0400

    Switch tabs

--- src/components/TabButtons.tsx
+++ src/components/TabButtons.tsx
@@ -1,10 +1,22 @@
+import { Dispatch, SetStateAction } from "react";
 import { Pet } from "../App";
 
-export default function TabButtons({ petData }: { petData: Pet[] }) {
+export default function TabButtons({
+  petData,
+  setActiveTab,
+}: {
+  petData: Pet[];
+  activeTab: number;
+  setActiveTab: Dispatch<SetStateAction<number>>;
+}) {
   return (
     <div className="tab__header">
-      {petData.map((item) => (
-        <li className={`tab__button`} key={item.animal}>
+      {petData.map((item, index) => (
+        <li
+          className={`tab__button`}
+          key={item.animal}
+          onClick={() => setActiveTab(index)}
+        >
           {item.animal}
         </li>
       ))}
--- src/components/TabContent.tsx
+++ src/components/TabContent.tsx
@@ -1,15 +1,17 @@
 import { Pet } from "../App";
 
-export default function TabContent({ petData }: { petData: Pet[] }) {
+export default function TabContent({
+  petData,
+  activeTab,
+}: {
+  petData: Pet[];
+  activeTab: number;
+}) {
   return (
     <div className="tab__container ">
       <div className="tab__content">
-        {petData.map((pet, index) => (
-          <div key={index}>
-            <img src={pet.image} alt="" />
-            <p>{pet.fact}</p>
-          </div>
-        ))}
+        <img src={petData[activeTab].image} alt="" />
+        <p> {petData[activeTab].fact}</p>
       </div>
     </div>
   );
